bfeff8f642c95ac3bfef582085c8c81d85dc9d81
Loz Jackson <lozjackson@talktalk.net>
2016-06-19 15:17:11 +0100

    update imports

--- addon/index.js
+++ addon/index.js
@@ -1,4 +1,5 @@
-import AutoSaveProxy, { save } from 'ember-auto-save/utils/auto-save';
+import AutoSaveProxy from 'ember-auto-save/utils/auto-save';
+import save from 'ember-auto-save/utils/auto-save';
 
 export default AutoSaveProxy;
 export { save };
