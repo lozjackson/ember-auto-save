ccb82cf6b1100215b20280e867d27e774a046091
Loz Jackson <lozjackson@talktalk.net>
2016-08-15 21:16:06 +0100

    update save method

--- tests/unit/utils/save-test.js
+++ tests/unit/utils/save-test.js
@@ -20,3 +20,18 @@ test('save method', function(assert) {
   save({ save: () => assert.ok(true) }, 0);
   Ember.run.debounce = _debounce;
 });
+
+test('save method - look for a model in the content property', function(assert) {
+  assert.expect(4);
+  let _debounce = Ember.run.debounce;
+  Ember.run.debounce = (ctx, fn, time) => {
+    assert.equal(typeof ctx, 'object');
+    assert.equal(typeof fn, 'function');
+    assert.equal(typeof time, 'number');
+    fn.call(ctx);
+  };
+  let model = { save: () => assert.ok(true) };
+  let _model = { content: model };
+  save(_model, 0);
+  Ember.run.debounce = _debounce;
+});
