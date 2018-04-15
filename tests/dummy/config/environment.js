7f9eae84fd836e67f48a7f1548c43ad919002633
Loz Jackson <lozjackson@talktalk.net>
2018-04-15 17:19:29 +0100

    Update config

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -29,13 +29,13 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
-    ENV.baseURL = '/';
+    ENV.rootURL = '/';
     ENV.locationType = 'auto';
   }
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
+    ENV.rootURL = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
